769009b454b1bf79645be12f49f241550acbd5e3
root <root@debianWheezy>
2014-06-24 11:35:25 -0400

    quitando archivos que ya no se usan mas

--- page.php
+++ page.php
@@ -2,6 +2,5 @@
 if ( is_page(2) ) { include('page_report.php'); }
 elseif ( is_page(8) ) { include('page_stories.php'); }
 elseif ( is_page(10) ) { include('page_maps.php'); }
-elseif ( is_page(12) ) { include('page_data.php'); }
 else{ include('page_about.php'); }
 ?>
