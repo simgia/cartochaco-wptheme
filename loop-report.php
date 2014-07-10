2a33e33061cf0d5e55e8c54ce565f338e7417b31
root <root@debianWheezy>
2014-07-10 17:23:27 -0400

    traduciones y arreglos para los informes

--- loop-report.php
+++ loop-report.php
@@ -5,12 +5,8 @@
 	        <li id="post-<?php the_ID(); ?>" <?php post_class('three columns'); ?>>
                     <div class="archivo">
 		        <h4><?php echo get_the_title(); ?></h4>
-			<p>
-                            <br>                          
-			    Terms of use: Lorem Ipsum
-                        </p>
 			<p class="brdTop">
-                            <a href="<?php the_permalink(); ?>"><?php _e('read me', 'jeo'); ?></a>
+                            <a href="<?php the_permalink(); ?>"><?php _e('Visualize', 'jeo'); ?></a>
 			</p>
                     </div>
 		</li>
