49e11918bed0ceaf6cf05d215e79c2332eb61b5d
simgia <josego@simgia.com>
2016-08-08 10:27:55 -0400

    adding dataset template

---
+++ archive-dataset.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+
+<section id="content">
+	<div id="map-archive" class="gray-page archive-page">
+		<?php 
+                    // Muestra 8 dataset por pagina.
+                    $paged = 1;
+                    if(get_query_var('paged')) $paged = get_query_var('paged');
+                    if(get_query_var('page')) $paged = get_query_var('page');
+                    $wp_query = new WP_Query(); 
+                    $wp_query->query('showposts=8&post_type=dataset'.'&paged='.$paged); 
+                    if($wp_query->have_posts()) :  
+                ?>
+
+			<section id="datasets" class="dataset-loop-section archive-list">
+				<div class="page-title row">
+					<div class="container">
+						<div class="twelve columns">
+							<h1><?php _e('Datasets', 'jeo'); ?></h1>
+						</div>
+					</div>
+				</div>
+
+				<div class="container">
+					<?php get_template_part('loop', 'dataset'); ?>
+				</div>
+			</section>
+		<?php endif; ?>
+	</div>
+</section>
+
+<?php get_footer(); ?>
