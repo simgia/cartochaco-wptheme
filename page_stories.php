791e232fa59caf4bc6fee1a24cea41e1f331f41d
simgia <josego@simgia.com>
2016-08-08 10:16:06 -0400

    adding mores pages maps stories

---
+++ page_stories.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+
+<?php if(have_posts()) : the_post(); ?>
+	<section id="content" class="single-post bblanco">
+		<header class="single-post-header">
+			<div class="container">
+				<div class="twelve columns">
+					<h1><?php the_title(); ?></h1>
+				</div>
+			</div>
+		</header>
+		<div class="container">
+			<div class="twelve columns">
+				<?php the_content(); ?>
+				<?php
+				wp_link_pages( array(
+					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'jeo' ) . '</span>',
+					'after'       => '</div>',
+					'link_before' => '<span>',
+					'link_after'  => '</span>',
+				) );
+				?>
+			</div>
+		</div>
+	</section>
+<?php endif; ?>
+
+<?php get_footer(); ?>
