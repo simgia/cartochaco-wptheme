b69df5fc0a648b9f8f88badb1ff4af8705da0833
simgia <josego@simgia.com>
2016-08-08 10:11:30 -0400

    loop for report

---
+++ loop-report.php
@@ -0,0 +1,16 @@
+<?php if(have_posts()) : ?>
+    <div class="container">
+        <ul class="">
+	    <?php while(have_posts()) : the_post(); ?>  
+	        <li id="post-<?php the_ID(); ?>" <?php post_class('three columns'); ?>>
+                    <div class="archivo">
+		        <h4><?php echo get_the_title(); ?></h4>
+			<p class="brdTop">
+                            <a href="<?php the_permalink(); ?>"><?php _e('Visualize', 'jeo'); ?></a>
+			</p>
+                    </div>
+		</li>
+	    <?php endwhile; ?>
+	</ul>
+	<?php if(function_exists('wp_paginate')) wp_paginate(); ?>
+<?php endif; ?>
